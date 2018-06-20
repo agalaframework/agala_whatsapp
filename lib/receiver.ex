93cc2f7a3ba9959bf1f43c7eb4f4b9516ce863db
Dmitry Rubinstein <rubinsteindb@gmail.com>
2018-06-21 00:43:34 +0300

    Fix receiver's get_updates params

--- lib/receiver.ex
+++ lib/receiver.ex
@@ -6,11 +6,12 @@ defmodule Agala.Provider.Whatsapp.Receiver do
   use Agala.Bot.Receiver
   alias Agala.BotParams
 
-  def get_updates(notify_with, %BotParams{private: %{in_queue: queue, channel: chan}}) do
+  def get_updates(notify_with, %BotParams{private: %{in_queue: queue, channel: chan}} = bot_params) do
     case AMQP.Basic.get(chan, queue, no_ack: true) do
       {:ok, data, _opts} -> notify_with.(:erlang.binary_to_term(data))
       {:empty, _opts} -> :do_nothing
       error -> Logger.error("Error while receiving updates for Whatsapp from RabbitMQ: #{error}")
     end
+    bot_params
   end
 end
